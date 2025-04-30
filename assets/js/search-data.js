bfda4239097639f494b52bd681d6820bf21e3da8
PunitGandhi <punit.gandhi@gmail.com>
2025-04-30 01:56:49 +0000

    Deploying to gh-pages from @ PunitGandhi/PunitGandhi.github.io@4605d445f76d7643b2fc4da119f88c0d2f909a3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -501,35 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%67%61%6E%64%68%69%70%72@%76%63%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=SsU2c9oAAAAJ&hl", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0000-0001-8812-7724", "_blank");
+        },
+      },{
+        id: 'social-arXiv',
+        title: 'Arxiv',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://arxiv.org/search/?query=Gandhi%2C+Punit&searchtype=all&source=header", "_blank");
         },
       },{
       id: 'light-theme',
